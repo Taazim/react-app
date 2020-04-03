ae8fa56304142fa69f23fd4ee85a25baf25379d5
Taazim <tdegembaeva@users.noreply.github.com>
2020-04-04 01:00:06 +0600

    Updates

---
+++ precache-manifest.7094205bf774f3623ff59fa1d15596fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98365e33bf16819f1e7987cfb40bc1e",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/index.html"
+  },
+  {
+    "revision": "cf1b917daddddcf0e289",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/static/css/main.f05cd5d3.chunk.css"
+  },
+  {
+    "revision": "6d70c443250b8015ab39",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/static/js/2.3415368c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/static/js/2.3415368c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1b917daddddcf0e289",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/static/js/main.0a169bc3.chunk.js"
+  },
+  {
+    "revision": "f771caf893e9db2cc62b",
+    "url": "/Taazim/my_list_todo.github.io/my-to-do-list/static/js/runtime-main.24fca522.js"
+  }
+]);
